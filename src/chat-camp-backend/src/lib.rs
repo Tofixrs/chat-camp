28564ab86032e0d38ccae44afe3a96c909d02ab6
Tofixrs <tofix.rs@proton.me>
2024-07-30 19:59:58 +0200

    basic chat

--- src/chat-camp-backend/src/lib.rs
+++ src/chat-camp-backend/src/lib.rs
@@ -1,37 +1,43 @@
 use core::panic;
-use std::{
-    cell::{Ref, RefCell},
-    collections::HashMap,
-    vec,
-};
+use std::{cell::RefCell, collections::HashMap, vec};
 
-use candid::{CandidType, Deserialize, Principal};
+use candid::Principal;
 use ic_cdk::caller;
 
 thread_local! {
-    static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default()
+    static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
 }
 
 #[ic_cdk::query]
-fn get_notes(user: Principal) -> Option<Vec<String>> {
-    NOTES.with_borrow(|notes| notes.get(&user).cloned())
-}
-
-#[ic_cdk::update]
-fn add_note(note: String) {
+fn get_chat(user_chat: Principal) -> Option<Vec<String>> {
     let user = caller();
     if user == Principal::anonymous() {
         panic!("Anon")
     }
 
-    NOTES.with_borrow_mut(|notes_hash| {
-        let notes = notes_hash.get_mut(&user);
-        match notes {
-            Some(notes_vec) => {
-                notes_vec.push(note);
+    let mut key = [user, user_chat];
+    key.sort();
+    CHAT.with_borrow(|chats| chats.get(&key).cloned())
+}
+
+#[ic_cdk::update]
+fn add_chat_msg(msg: String, send_to: Principal) {
+    let user = caller();
+    if user == Principal::anonymous() {
+        panic!("Anon")
+    }
+
+    let mut key = [user, send_to];
+    key.sort();
+
+    CHAT.with_borrow_mut(|chats| {
+        let chat = chats.get_mut(&key);
+        match chat {
+            Some(chat) => {
+                chat.push(msg);
             }
             None => {
-                notes_hash.insert(user, vec![note]);
+                chats.insert(key, vec![msg]);
             }
         }
     })
--- src/chat-camp-frontend/src/App.vue
+++ src/chat-camp-frontend/src/App.vue
@@ -1,56 +1,116 @@
 <script lang="ts">
-import { ref } from 'vue';
 import { createActor, canisterId } from '../../declarations/chat-camp-backend/index';
 import { AuthClient } from '@dfinity/auth-client';
-import { HttpAgent, type Identity } from '@dfinity/agent';
+import { type Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import { watch, watchEffect } from 'vue';
 
 export default {
   data() {
     return {
-      newNote: "",
-      notes: [] as string[],
+      newMsg: "",
+      chat: [] as string[],
       identity: undefined as Identity | undefined,
-      backend: createActor(canisterId)
+      backend: createActor(canisterId),
+      recipient: "",
     }
   },
   methods: {
-    async addNote() {
-      await this.backend.add_note(this.newNote);
-      this.newNote = ""
-      await this.getNotes();
+    async addMsg() {
+      const text = this.newMsg;
+      this.newMsg = ""
+      await this.backend.add_chat_msg(text, this.getPrincipal())
+      await this.getChat();
     },
-    async getNotes() {
+    async getChat() {
+      if (!this.isLoggedIn()) {
+        throw new Error("Anon")
+      };
+      if (!this.isTalkingToSomeone()) {
+        throw new Error("Didnt specify who ya talking  to");
+      }
+
+      const [chat] = await this.backend.get_chat(this.getPrincipal())
+      this.chat = chat ? chat : [];
     },
     async login() {
       const authClient = await AuthClient.create();
       await authClient.login({
         identityProvider: "http://a3shf-5eaaa-aaaaa-qaafa-cai.localhost:4943/",
+        onSuccess: async () => {
+          this.identity = authClient.getIdentity();
+          this.backend = createActor(canisterId, { agentOptions: { identity: this.identity } })
+        },
       });
-      this.identity = authClient.getIdentity();
-      const agent = await HttpAgent.create({ identity: this.identity })
-      this.backend = createActor(canisterId, { agent })
-    }
-  },
-  mounted() {
-    this.getNotes()
+    },
+    isLoggedIn() {
+      return !!this.identity && !this.identity.getPrincipal().isAnonymous()
+    },
+    isTalkingToSomeone() {
+      return this.recipient != ""
+    },
+    getPrincipal() {
+      const principal = Principal.fromText(this.recipient.trim());
+      if (!principal || principal.isAnonymous()) {
+        throw "Anon"
+      }
+
+      return principal
+    },
   },
 }
-
 </script>
 
+<style>
+nav {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 5px 10px;
+  background-color: darkgray;
+}
+
+.nav-start,
+.nav-center,
+.nav-end {
+  display: flex;
+  align-items: center;
+}
+
+nav .nav-end {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
+
 <template>
-  <main>
-    <img src="/logo2.svg" alt="DFINITY logo" />
-    <br />
-    <br />
-    <button @click="login">Login</button>
-    {{ identity?.getPrincipal() }}
-    <div v-for="note in notes">
-      <p>{{ note }}</p>
-    </div>
-    <div>
-      <textarea v-model="newNote"></textarea>
-      <button @click="addNote">Dodaj notke</button>
+  <header>
+    <nav>
+      <div class="nav-start">
+        <i class="fa-regular fa-user"></i>
+        {{ identity?.getPrincipal() ? identity.getPrincipal() : "Not logged in" }}
+      </div>
+      <div class="nav-center"></div>
+      <div class="nav-end">
+        <button @click="login">Login</button>
+      </div>
+    </nav>
+  </header>
+  <main class="msgs">
+    <div class="add-note-form">
+      <div>
+        <label for="recipient">Recipient</label>
+        <input type="text" id="recipient" v-model="recipient">
+        <button @click="getChat">Refresh</button>
+      </div>
+      <div v-for="msg in chat" class="note">
+        <p>{{ msg }}</p>
+      </div>
+      <div>
+        <textarea v-model="newMsg"></textarea>
+      </div>
+      <div>
+        <button @click="addMsg" :disabled="!isLoggedIn() || !isTalkingToSomeone()">Dodaj notke</button>
+      </div>
     </div>
   </main>
 </template>
