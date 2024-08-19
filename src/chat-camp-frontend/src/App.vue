516aeb14c6d68e9e49a353f7df667108f2208c29
tofixrs <tofix.rs@proton.me>
2024-08-19 14:05:55 +0000

    fix: register being broken

--- src/chat-camp-frontend/src/App.vue
+++ src/chat-camp-frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Header from "./components/Header.vue"
+import Header from "./components/Header.vue";
 import Messages from "./components/Messages.vue";
 import UserList from "./components/UserList.vue";
 import RegisterPage from "./components/RegisterPage.vue";
@@ -9,31 +9,34 @@ import { useRecipientData } from "./stores/recipientData";
 const loginData = useLoginData();
 const recipientData = useRecipientData();
 loginData.restoreLogin();
-
 </script>
 
-
 <style>
-main {
-    flex-grow: 1;
-    display: flex;
-    max-height: 100%;
+.main {
+  flex-grow: 1;
+  display: flex;
+  max-height: 100%;
 }
 
 .split {
-    display: grid;
-    flex-grow: 1;
-    grid-template-columns: 0.1fr 1fr;
+  display: grid;
+  flex-grow: 1;
+  grid-template-columns: 0.1fr 1fr;
 }
 </style>
 
 <template>
-    <Header />
-    <main v-if="loginData.identity != undefined && loginData.userData != undefined">
-        <div class="split" v-if="loginData.isLoggedIn()">
-            <UserList />
-            <Messages v-if="recipientData.recipient != ''" />
-        </div>
-    </main>
-    <RegisterPage v-if="loginData.identity != undefined && loginData.userData == undefined" />
+  <Header />
+  <main
+    class="main"
+    v-if="loginData.identity != undefined && loginData.userData != undefined"
+  >
+    <div class="split" v-if="loginData.isLoggedIn()">
+      <UserList />
+      <Messages v-if="recipientData.recipient != ''" />
+    </div>
+  </main>
+  <RegisterPage
+    v-if="loginData.identity != undefined && loginData.userData == undefined"
+  />
 </template>
